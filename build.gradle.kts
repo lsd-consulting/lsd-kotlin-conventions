6ca4f823c7b1b5aa212d36d1e24294c87b021915
Nick McDowall <nicholasinengland@hotmail.com>
2025-07-26 20:14:31 +0100

    feat: add LSD Kotlin conventions plugins
    - Add lsd.library plugin for Java library conventions
    - Add lsd.kotlin-library plugin extending lsd.library with Kotlin features
    - Include Dokka documentation and JaCoCo test coverage
    - Configure Maven Central publishing with signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
 plugins {
     `kotlin-dsl`
     `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("com.palantir.git-version") version "4.0.0"
 }
 
 group = "io.github.lsd-consulting"
-version = "1.0.0"
+val gitVersion: groovy.lang.Closure<String> by extra
+version = gitVersion().replace(Regex("^v"), "")
+
+logger.lifecycle("Version: $version")
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -39,6 +45,74 @@ gradlePlugin {
         create("lsdKotlinLibrary") {
             id = "lsd.kotlin-library"
             implementationClass = "lsd.conventions.LsdKotlinLibraryPlugin"
+        }
+    }
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            pom {
+                name.set("LSD Kotlin Conventions")
+                description.set("Gradle convention plugins for LSD projects.")
+                url.set("https://github.com/lsd-consulting/lsd-kotlin-conventions")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/lsd-consulting/lsd-kotlin-conventions/blob/main/LICENSE")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("nickmcdowall")
+                        name.set("Nick")
+                        email.set("nicholas.mcdowall@gmail.com")
+                    }
+                    developer {
+                        id.set("lukaszgryzbon")
+                        name.set("Lukasz")
+                        email.set("lukasz.gryzbon@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git@github.com:lsd-consulting/lsd-kotlin-conventions.git")
+                    developerConnection.set("scm:git:git@github.com:lsd-consulting/lsd-kotlin-conventions.git")
+                    url.set("https://github.com/lsd-consulting/lsd-kotlin-conventions")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "ossrh-staging-api"
+            url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
+            credentials(PasswordCredentials::class)
+        }
+    }
+}
+
+signing {
+    if (project.findProperty("signingKey") != null) {
+        // Use in-memory ascii-armored keys
+        val signingKey: String? = findProperty("signingKey") as String?
+        val signingPassword: String? = findProperty("signingPassword") as String?
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        sign(publishing.publications)
+    } else {
+        // Use signing properties in ~/.gradle/gradle.properties
+        sign(publishing.publications)
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
         }
     }
 }
