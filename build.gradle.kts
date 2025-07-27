95dada77c2f008bb99f82a632f81b8ebb9c6d49e
Nick McDowall <nicholasinengland@hotmail.com>
2025-07-27 09:21:55 +0100

    fix: add sources and javadoc JARs for Maven Central compliance

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ logger.lifecycle("Version: $version")
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
+    withJavadocJar()
 }
 
 kotlin {
